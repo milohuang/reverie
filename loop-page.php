674ec78a40f1e21c8f302dce1d56539a4afdd853
Sherwin Techico <stechico@gmail.com>
2011-12-04 07:11:05 -0800

    Updated markup to match the rest of the pages (e.g. horizontal line, and .sidebar-box instead of .container).

--- loop-page.php
+++ loop-page.php
@@ -5,4 +5,4 @@
 	<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'reverie'), 'after' => '</p></nav>' )); ?>
 <?php endwhile; // End the loop ?>
 
-<?php edit_post_link('Edit', '<p>[ ', ' ]</p>'); ?>
+<?php edit_post_link('Edit', '<p>(', ')</p>'); ?>
