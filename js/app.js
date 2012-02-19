03be514253ea99a5c1cc683bed7969a4dbde8cdd
Pete Schuster <pete@peteschuster.com>
2012-02-19 09:37:25 -0500

    updated js to use jQuery in no conflict mode.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /* Foundation v2.1.4 http://foundation.zurb.com */
-$(document).ready(function () {
+jQuery( document ).ready(function () {
 
 	/* Use this js doc for all application specific JS */
 
@@ -15,27 +15,27 @@ $(document).ready(function () {
 		$tab.addClass('active');
 
     	//Show Tab Content
-		$(contentLocation).closest('.tabs-content').children('li').hide();
-		$(contentLocation).show();
+		jQuery(contentLocation).closest('.tabs-content').children('li').hide();
+		jQuery(contentLocation).show();
 	}
 
-	$('dl.tabs').each(function () {
+	jQuery('dl.tabs').each(function () {
 		//Get all tabs
-		var tabs = $(this).children('dd').children('a');
+		var tabs = jQuery(this).children('dd').children('a');
 		tabs.click(function (e) {
-			activateTab($(this));
+			activateTab(jQuery(this));
 		});
 	});
 
 	if (window.location.hash) {
-		activateTab($('a[href="' + window.location.hash + '"]'));
+		activateTab(jQuery('a[href="' + window.location.hash + '"]'));
 	}
 
 	/* ALERT BOXES ------------ */
-	$(".alert-box").delegate("a.close", "click", function(event) {
+	jQuery(".alert-box").delegate("a.close", "click", function(event) {
     event.preventDefault();
-	  $(this).closest(".alert-box").fadeOut(function(event){
-	    $(this).remove();
+	  jQuery(this).closest(".alert-box").fadeOut(function(event){
+	    jQuery(this).remove();
 	  });
 	});
 
@@ -43,48 +43,48 @@ $(document).ready(function () {
 	/* PLACEHOLDER FOR FORMS ------------- */
 	/* Remove this and jquery.placeholder.min.js if you don't need :) */
 
-	$('input, textarea').placeholder();
+	jQuery('input, textarea').placeholder();
 
 
 
 	/* UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE6/7/8 SUPPORT AND ARE USING .block-grids */
-//	$('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
-//	$('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
-//	$('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
-//	$('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
+//	jQuery('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
+//	jQuery('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
+//	jQuery('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
+//	jQuery('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
 
 
 
 	/* DROPDOWN NAV ------------- */
 
 	var currentFoundationDropdown = null;
-	$('.nav-bar li a').each(function() {
-		$(this).data('clicks', 0);
+	jQuery('.nav-bar li a').each(function() {
+		jQuery(this).data('clicks', 0);
 	});
-	$('.nav-bar li a').on('click', function(e) {
+	jQuery('.nav-bar li a').on('click', function(e) {
 		e.preventDefault();
-		if (currentFoundationDropdown !== $(this).index() || currentFoundationDropdown === null) {
-			$(this).data('clicks', 0);
-			currentFoundationDropdown = $(this).index();
+		if (currentFoundationDropdown !== jQuery(this).index() || currentFoundationDropdown === null) {
+			jQuery(this).data('clicks', 0);
+			currentFoundationDropdown = jQuery(this).index();
 		}
-		$(this).data('clicks', ($(this).data('clicks') + 1));
-		var f = $(this).siblings('.flyout');
-		if (!f.is(':visible') && $(this).parent('.has-flyout').length > 1) {
-			$('.nav-bar li .flyout').hide();
+		jQuery(this).data('clicks', (jQuery(this).data('clicks') + 1));
+		var f = jQuery(this).siblings('.flyout');
+		if (!f.is(':visible') && jQuery(this).parent('.has-flyout').length > 1) {
+			jQuery('.nav-bar li .flyout').hide();
 			f.show();
-		} else if (($(this).data('clicks') > 1) || ($(this).parent('.has-flyout').length < 1)) {
-			window.location = $(this).attr('href');
+		} else if ((jQuery(this).data('clicks') > 1) || (jQuery(this).parent('.has-flyout').length < 1)) {
+			window.location = jQuery(this).attr('href');
 		}
 	});
-	$('.nav-bar').on('click', function(e) {
+	jQuery('.nav-bar').on('click', function(e) {
 		e.stopPropagation();
-		if ($(e.target).parents().is('.flyout') || $(e.target).is('.flyout')) {
+		if (jQuery(e.target).parents().is('.flyout') || jQuery(e.target).is('.flyout')) {
 			e.preventDefault();
 		}
 	});
-	// $('body').bind('touchend', function(e) {
-	// 	if (!$(e.target).parents().is('.nav-bar') || !$(e.target).is('.nav-bar')) {
-	// 		$('.nav-bar li .flyout').is(':visible').hide();
+	// jQuery('body').bind('touchend', function(e) {
+	// 	if (!jQuery(e.target).parents().is('.nav-bar') || !jQuery(e.target).is('.nav-bar')) {
+	// 		jQuery('.nav-bar li .flyout').is(':visible').hide();
 	// 	}
 	// });
 
