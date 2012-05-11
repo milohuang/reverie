fed454366baa171681655508a22f0c5a2a60fa8e
FLiCKaPiX DeZign <webmaster@flickapix.co.uk>
2012-05-11 15:09:46 +0200

    replaced h1 tags with h2

--- loop-single.php
+++ loop-single.php
@@ -2,7 +2,7 @@
 <?php while (have_posts()) : the_post(); ?>
 	<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 		<header>
-			<h1 class="entry-title"><?php the_title(); ?></h1>
+			<h2 class="entry-title"><?php the_title(); ?></h2>
 			<?php reverie_entry_meta(); ?>
 		</header>
 		<div class="entry-content">
