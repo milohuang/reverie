5bdf55b206ae881990590ad086d9e2be7e04ef33
FLiCKaPiX DeZign <webmaster@flickapix.co.uk>
2012-06-20 02:24:04 +0200

    removed js and enqueued from functions.php

--- footer.php
+++ footer.php
@@ -16,10 +16,6 @@
 			
 	</div><!-- Container End -->
 	
-	<!-- Included JS Files of Foundation -->
-	<script src="<?php echo get_template_directory_uri(); ?>/js/foundation.js"></script>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/app.js"></script>
-	
 	<!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
 	     chromium.org/developers/how-tos/chrome-frame-getting-started -->
 	<!--[if lt IE 7]>
