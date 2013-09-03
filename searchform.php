d69bc68ce7ca45180ac17e1094a8e653350a2f76
Paul Gibbs <paul@byotos.com>
2013-09-03 11:49:56 +0100

    Correctly escape string translations in form elements

--- searchform.php
+++ searchform.php
@@ -1,10 +1,10 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
 	<div class="row collapse">
 		<div class="small-8 columns">
-			<input type="text" value="" name="s" id="s" placeholder="<?php _e('Search', 'reverie'); ?>">
+			<input type="text" value="" name="s" id="s" placeholder="<?php esc_attr_e('Search', 'reverie'); ?>">
 		</div>
 		<div class="small-4 columns">
-			<input type="submit" id="searchsubmit" value="<?php _e('Search', 'reverie'); ?>" class="prefix button">
+			<input type="submit" id="searchsubmit" value="<?php esc_attr_e('Search', 'reverie'); ?>" class="prefix button">
 		</div>
 	</div>
 </form>
