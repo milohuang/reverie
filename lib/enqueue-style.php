bbda942a3e4d5d247dc680b71c0b9eefe5756264
Chris Violette <cvioletterun@gmail.com>
2014-02-04 14:00:26 -0500

    Reversed previous commit; using milohuang/reverie-child renders this step edit unnecessary.

--- lib/enqueue-style.php
+++ lib/enqueue-style.php
@@ -7,10 +7,10 @@ if( ! function_exists( 'reverie_enqueue_style' ) ) {
 	function reverie_enqueue_style()
 	{
 		// foundation stylesheet
-		wp_register_style( 'reverie-foundation-stylesheet', get_template_directory_uri() . '/css/app.css', array(), '' );
+		wp_register_style( 'reverie-foundation-stylesheet', get_stylesheet_directory_uri() . '/css/app.css', array(), '' );
 
 		// Register the main style
-		wp_register_style( 'reverie-stylesheet', get_template_directory_uri() . '/css/style.css', array(), '', 'all' );
+		wp_register_style( 'reverie-stylesheet', get_stylesheet_directory_uri() . '/css/style.css', array(), '', 'all' );
 		
 		wp_enqueue_style( 'reverie-foundation-stylesheet' );
 		wp_enqueue_style( 'reverie-stylesheet' );
